a7f1832c3a712f0b3602b0eaa8a6d8294e69feea
Baymaxawa <2502474438@qq.com>
2026-05-02 23:41:46 +0800

    feat: add adapt confirm dialog and some functions

--- scripts/lang_selection.gd
+++ scripts/lang_selection.gd
@@ -11,12 +11,24 @@ extends Control
 @onready var shark_idle_player: AnimationPlayer = $SharkIDLEPlayer
 @onready var shark_anim_player: AnimationPlayer = $SharkAnimPlayer
 @onready var advice_timer: Timer = $AdviceTimer
+@onready var adapt_no_button: TextureButton = $StartGamePrompt/NoButton
 
 var restore_chinese: bool = false
 var restore_japanese: bool = false
 var restore_english: bool = false
 
 var click_counter: int = 0
+var no_counter: int = 0
+
+var min_button_pos: Vector2 = Vector2(0.0, 800.0)
+var max_button_pos: Vector2 = Vector2(622.0, 1502.0)
+
+var 滑走的音效: Array = [
+	preload("uid://cvm7apr8mgu7v"),
+	preload("uid://nqi0b0acynvn"),
+	preload("uid://biihulnfelfa0"),
+	preload("uid://dqnohf8td3mug"),
+]
 
 const 小鲨鱼被点击 = preload("uid://bawkxkdwd5v8c")
 const 我觉得要不选中文吧_ = preload("uid://cbdtq71l4rp")
@@ -78,6 +90,7 @@ func _on_japanese_pressed() -> void:
 	shark.play("default")
 
 func _on_chinese_button_pressed() -> void:
+	shark_button.disabled = true
 	advice_timer.paused = true
 	animation_player.play("Chinese")
 	await animation_player.animation_finished
@@ -131,3 +144,25 @@ func _on_advice_timer_timeout() -> void:
 		chinese_button.disabled = false
 		restore_chinese = false
 	shark.play(save)
+
+func _on_adapt_yes_button_pressed() -> void:
+	print("User agreed to adapt the shark")
+	# TODO: Transition effect and bump to next game scene (WIP)
+
+func _on_adapt_no_button_pressed() -> void:
+	print("Disagree count: "+str(no_counter))
+	if no_counter >= 7:
+		adapt_no_button.disabled = true
+		return
+	adapt_no_button.disabled = true
+	no_counter += 1
+	var tween: Tween = get_tree().create_tween()
+	var rand_x = randf_range(min_button_pos.x, max_button_pos.x)
+	var rand_y = randf_range(min_button_pos.y, max_button_pos.y)
+	var rand_pos: Vector2 = Vector2(rand_x, rand_y)
+	print("Generated random position: "+str(rand_pos))
+	tween.tween_property(adapt_no_button, "position", rand_pos, 0.75).set_trans(Tween.TRANS_LINEAR)
+	audios.stream = 滑走的音效[randi_range(0, 3)]
+	audios.play()
+	await tween.finished
+	adapt_no_button.disabled = false
